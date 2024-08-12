ac26958ff77e46044d9453a5dfc18bce7261f48a
syperjet <syperjet@Ubuntu.myguest.virtualbox.org>
2024-08-12 18:06:47 +0300

    hidden sensitive data from settings

--- movies_admin/config/settings.py
+++ movies_admin/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
 
 INTERNAL_IPS = [
     '127.0.0.1', 
