82861bdce4500298dd3380cc5d43f839e0b379bf
syperjet <syperjet@Ubuntu.myguest.virtualbox.org>
2024-08-12 19:14:01 +0300

    split mixins and momodels

--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -1,26 +1,8 @@
-import uuid
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+
+from django.core.validators import MinValueValidator, MaxValueValidator
+from .mixins import UUIDMixin, TimeStampedMixin
 from django.utils.translation import gettext_lazy as _
-
-
-class TimeStampedMixin(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        abstract = True
-
-
-class UUIDMixin(models.Model):
-    id = models.UUIDField(
-        primary_key=True, 
-        default=uuid.uuid4, 
-        editable=False
-        )
-    
-    class Meta:
-        abstract = True
 
 
 class Genre(UUIDMixin, TimeStampedMixin):
